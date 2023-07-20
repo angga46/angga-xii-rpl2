67ef246108c4a7aec379cfa2f64e6b03cce87d42
mrival21 <rivalnugraha656@gmail.com>
2023-07-20 15:48:10 +0700

    latihan vue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<center></center>
+<script setup>
+import { ref } from 'vue'
+
+let message = "Ini warna merah"
+let rawHtml = "<li>Gimana Kabarmu lek?</li>"
+
+</script>
+
+<template>
+  <div id="app">
+    <h1>{{ message }}</h1>
+    <span v-html="rawHtml"></span>
+    <span v-html="message" style="color: red"></span> <br>
+    <button :disabled="isButtonDisabled" onclick="alert('mana hutang mu?')">Baek</button>
+    <button :disabled="isButtonDisabled" onclick="alert('kabar baik? tapi hutang ga dibayar? MANA PATEN')">Allhamdulillah lek</button> <br>
+
+    <a v-bind:href="url">Klik lah lek link nya</a> <br>
+    
+    <input type="email" placeholder="Masukkan email anda"> <br>
+    <input type="password" placeholder="Masukkan password">
+  </div>
+</template>
